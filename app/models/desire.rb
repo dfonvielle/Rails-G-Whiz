7f77054408efb088b6e827c1711c6b7ca2ae54db
David Fonvielle <dfonvielle@hotmail.com>
2011-11-29 12:35:22 -0600

    Show user stories and business logic on project page and messages if none yet created

--- app/models/desire.rb
+++ app/models/desire.rb
@@ -2,5 +2,8 @@ class Desire < ActiveRecord::Base
   
   belongs_to :project
   
+  scope :user_stories, where(:desire_type => "User Stories")
+  scope :business_logic, where(:desire_type => "Business Logic")
+  
   
 end
