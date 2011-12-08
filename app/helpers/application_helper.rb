55cce876529e55b7352cd97d745f67f742c92ee2
David Fonvielle <dfonvielle@hotmail.com>
2011-12-07 18:56:53 -0600

    Cleaned up views and order of M-V-C interaction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+
+
+
+
+def yes_or_no (value)
+  
+  if value == true
+    "Yes"
+  else
+    "No"
+  end
+
+end
+
+
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -6,15 +6,8 @@ class Project < ActiveRecord::Base
   
   has_many :parts
 
-# def coolnews
-#   
-#   if @desires.blank?
-#     :message => "STEP 1: Enter user stories and business logic so that you'll know what you want to achieve with your rails app. In the next step, you'll identify parts of your rails app based off of the user stories and business logic that you enter."
-#   else
-#     :message => "Good job."  
-#   end
-#   
-# end 
+
+
   
   
 end
