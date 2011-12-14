9d745490453cf6bf9112c05d80d18b2513573dbc
David Fonvielle <dfonvielle@hotmail.com>
2011-12-13 21:28:12 -0600

    Enabled model associations to be viewed on project show page with links to add/modify actions and adding/editing notes

--- app/models/model_association.rb
+++ app/models/model_association.rb
@@ -1,2 +1,15 @@
 class ModelAssociation < ActiveRecord::Base
+  
+  belongs_to :association_type
+  belongs_to :project
+  
+  belongs_to :primary_model,
+              :class_name => "Part",
+              :foreign_key => "primary_model_id"
+              
+  belongs_to :secondary_model,
+              :class_name => "Part",
+              :foreign_key => "secondary_model_id"
+    
+  
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -6,6 +6,14 @@ class Part < ActiveRecord::Base
   has_many :controller_actions
   has_one :note
   
+  has_many :primary_model_associations,
+            :class_name => "ModelAssociation",
+            :foreign_key => "primary_model_id"
+              
+  has_many :secondary_model_associations,
+            :class_name => "ModelAssociation",
+            :foreign_key => "secondary_model_id"
+  
   scope :app_models, where(:model_role => true)
   scope :app_controllers, where(:controller_role => true)
   scope :app_views, where(:view_role => true)
